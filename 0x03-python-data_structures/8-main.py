81363f0462123685178d1c81ac1e19fd1c0b4cc6
dauwesh <dauwesh999@gmail.com>
2023-12-06 02:58:17 -0800

    latest

---
+++ 0x03-python-data_structures/8-main.py
@@ -0,0 +1,7 @@
+guillaume@ubuntu:~/0x03$ cat 8-main.py
+#!/usr/bin/python3
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
